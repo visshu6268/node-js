3b3d1a0c5ae90368def1071f2928d1885e80f82d
visshu6268 <vishalchauhan7097@gmail.com>
2024-08-07 23:48:58 +0530

    you

--- data.js
+++ data.js
@@ -1,12 +1,13 @@
 
 
-const emp={
-    name:"vishal",
-    salary:20000,
-    city:"gwalior",
-    itm:["vsihal",12000,gwalior],
-    mpct:function(){
-console.log("pninfosys")
+const emp = {
+    name: "vishal",
+    salary: 20000,
+    city: "gwalior",
+    itm: ["vsihal", 12000, "gwalior",1],
+    mpct: function () {
+        console.log("pninfosys")
     }
 }
-module.exports=data
+
+module.exports = data;
